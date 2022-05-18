55e1a3c71f2a27dfd16213315773a3bde34c133a
DarkDhz <blazegammer24@gmail.com>
2022-05-18 16:35:33 +0200

    added mysql credentials

---
+++ src/main/java/es/arnaugris/utils/MySQLUtils.java
@@ -0,0 +1,12 @@
+package es.arnaugris.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLUtils {
+
+    private String ip = "54.36.191.29";
+    private String username = "ubmail";
+    private String password = "ubmail";
+    private int port = 3306;
+}
